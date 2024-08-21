4a1ec15f8d6e859f3b56fc6e1d4f2735a8278e12
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-08-21 17:46:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr_block" {
+  description = "The top-level CIDR block for the VPC."
+  default     = "10.1.0.0/16"
+}
+
+variable "cidr_blocks" {
+  description = "The CIDR blocks to create the workstations in."
+  default     = ["10.1.1.0/24", "10.1.2.0/24"]
+}
+
+variable "environment" {
+  description = "Deployment Environment"
+  default     = "development"
+}
