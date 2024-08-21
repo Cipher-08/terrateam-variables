6f4310155f563017e838c90f3a5cfc05e2263fa8
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-08-21 23:12:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,45 +9,11 @@ provider "aws" {
 
   }
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-data "aws_availability_zones" "available" {
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
-
-  name                    = "firefly-vpc"
-  cidr                    = "10.42.0.0/16"
-  azs                     = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnets          = ["10.42.0.0/24", "10.42.1.0/24"]
-  map_public_ip_on_launch = true
-
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.image_id
+  ami                    = "ami-0e86e20dae9224db8"
   instance_type          = "t3.micro"
-  subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-
-  key_name = ""
 
   tags = {
     "Environment" = var.environment
